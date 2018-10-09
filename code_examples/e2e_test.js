2e6d65e250f618bbc28ae1d6f372f4cb292b485f
Brooke Mitchell <mitchell.brooke@gmail.com>
2018-10-09 19:13:58 +1300

    Tidy up notes

--- code_examples/e2e_test.js
+++ code_examples/e2e_test.js
@@ -2,10 +2,9 @@ test("Sucessful login shows the homepage", async function() {
   // Press the Login Button - opens menu
   await element(by.id("LoginButton")).press();
   // Enter user
-  await element(by.id("LoginEmail")).replaceText("test@email.in");
+  await element(by.id("LoginEmail")).inputText("test@email.in");
   // Enter password
-  await element(by.id("LoginPassword")).replaceText("excellentpassword");
-
+  await element(by.id("LoginPassword")).inputText("excellentpassword");
   // Press submit
   await element(by.id("SubmitButton")).press();
 
